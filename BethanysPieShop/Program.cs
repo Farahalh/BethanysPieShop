5065d559b10221913ee30232334fa56fb86621c7
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-10-31 18:33:09 +0100

    Configuring the application

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,6 +1,24 @@
+// Create a WebApplicationBuilder instance with command-line arguments.
 var builder = WebApplication.CreateBuilder(args);
+
+// Register the services needed for controllers and views.
+builder.Services.AddControllersWithViews();
+
+// Build the WebbApplication.
 var app = builder.Build();
 
+// Enable serving of static files (e.g., CSS, JavaScript, images) in the application.
+app.UseStaticFiles();
+
+// Check if the application is running in the development environment.
+if (app.Environment.IsDevelopment())
+{
+    // If in development mode, use a developer-friendly exception page for error handling.
+    app.UseDeveloperExceptionPage();
+}
+
+// Define a route for handling HTTP GET requests at the root URL ("/").
 app.MapGet("/", () => "Hello World!");
 
+// Start the application and listen for incoming HTTP requests.
 app.Run();
